2dddf3d867566ed2c0fbab4d92b090ad3a009f71
Alikhan <auten384@gmail.com>
2026-07-29 15:10:18 +0500

    chore: increase memory limits for Node.js in staging and production environments

--- prisma/generated/edge.js
+++ prisma/generated/edge.js
@@ -525,7 +525,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/index.js
+++ prisma/generated/index.js
@@ -526,7 +526,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
